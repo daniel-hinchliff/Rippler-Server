29038f3efdbe71cd3f0d946678d2dbdbfcb9f487
daniel-hinchliff <danmath01@yahoo.com.ar>
2016-07-16 23:41:26 +0100

    Print base url on home page

--- app/routes/home.php
+++ app/routes/home.php
@@ -1,6 +1,10 @@
 <?php
 
-$app->get('/', function() {
-    echo "Rippler Server";
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+$app->get('/', function (ServerRequestInterface $request, ResponseInterface $response) {
+    echo "Rippler Server <br>";
+    echo "Base Url: ", $request->getUri()->getBaseUrl();
 });
 
