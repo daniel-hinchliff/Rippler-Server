8b204015f8d01048eee79977e26dd53f83ee4f09
daniel-hinchliff <danmath01@yahoo.com.ar>
2016-07-16 16:19:33 +0100

    Add route to get facebook token

--- app/routes.php
+++ app/routes.php
@@ -4,3 +4,4 @@ require 'routes/home.php';
 require 'routes/swipe.php';
 require 'routes/image.php';
 require 'routes/ripple.php';
+require 'routes/token.php';
---
+++ app/routes/token.php
@@ -0,0 +1,43 @@
+<?php
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+$app->get('/token', function (ServerRequestInterface $request, ResponseInterface $response) {
+
+    $app_id = "423432704517027";
+    $app_secret = "b23059f536307fa4ebe8d4a5e6ba7d11";
+
+    $fb = new Facebook\Facebook([
+      'app_id' => $app_id,
+      'app_secret' => $app_secret,
+      'default_graph_version' => 'v2.2',
+    ]);
+
+    $helper = $fb->getRedirectLoginHelper();
+
+    $url = 'https://rippler-backend.herokuapp.com/token/print';
+
+    $loginUrl = $helper->getLoginUrl($url, ['email']);
+
+    echo '<a href="' . htmlspecialchars($loginUrl) . '">Get Token</a>';
+});
+
+$app->get('/token/print', function (ServerRequestInterface $request, ResponseInterface $response) {
+
+    $app_id = "423432704517027";
+    $app_secret = "b23059f536307fa4ebe8d4a5e6ba7d11";
+
+    $fb = new Facebook\Facebook([
+      'app_id' => $app_id,
+      'app_secret' => $app_secret,
+      'default_graph_version' => 'v2.2',
+    ]);
+
+    $helper = $fb->getRedirectLoginHelper();
+
+    echo $helper->getAccessToken();
+});
+
+
+
