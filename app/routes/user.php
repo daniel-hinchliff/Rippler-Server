7629f3d763778d54dad57fb3f941ac2ac06efe63
daniel-hinchliff <danmath01@yahoo.com.ar>
2016-07-23 14:07:00 +0100

    Restrict access to /user to authenticated users

--- app/routes/user.php
+++ app/routes/user.php
@@ -1,17 +1,22 @@
 <?php
 
 use Rippler\Models\User;
+use Rippler\Components\Auth;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
-$app->get('/user', function (ServerRequestInterface $request, ResponseInterface $response) {
-    return $response->withJson(User::all());
-});
+$app->group('/user', function () {
 
-$app->get('/me', function (ServerRequestInterface $request, ResponseInterface $response) {
+    $this->get('', function (ServerRequestInterface $request, ResponseInterface $response) {
+        return $response->withJson(User::all());
+    });
 
-    if (isset($_SESSION['user_id']))
-    {
-        return $response->withJson(User::find($_SESSION['user_id']));
-    }
-});
+    $this->get('/me', function (ServerRequestInterface $request, ResponseInterface $response) {
+
+        if (isset($_SESSION['user_id']))
+        {
+            return $response->withJson(User::find($_SESSION['user_id']));
+        }
+    });
+
+})->add(Auth::class);
