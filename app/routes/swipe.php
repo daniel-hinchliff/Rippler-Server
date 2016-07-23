cabfdc59246aa17067e71b76912568319b4c6b96
daniel-hinchliff <danmath01@yahoo.com.ar>
2016-07-23 14:05:45 +0100

    Add route group to swipes

--- app/routes/swipe.php
+++ app/routes/swipe.php
@@ -4,17 +4,21 @@ use Rippler\Models\Swipe;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
-$app->get('/swipe', function (ServerRequestInterface $request, ResponseInterface $response) {
-    return $response->withJson(Swipe::all());
+$app->group('/swipe', function () {
+
+    $this->get('', function (ServerRequestInterface $request, ResponseInterface $response) {
+        return $response->withJson(Swipe::all());
+    });
+
+    $this->post('', function (ServerRequestInterface $request, ResponseInterface $response) {
+
+        $attributes = $request->getParsedBody();
+
+        $ripple = new Swipe();
+        $ripple->ripple_id = $attributes['ripple_id'];
+        $ripple->like = $attributes['like'];
+        $ripple->user_id = 1;
+        $ripple->save();
+    });
 });
 
-$app->post('/swipe', function (ServerRequestInterface $request, ResponseInterface $response) {
-
-    $attributes = $request->getParsedBody();
-
-    $ripple = new Swipe();
-    $ripple->ripple_id = $attributes['ripple_id'];
-    $ripple->like = $attributes['like'];
-    $ripple->user_id = 1;
-    $ripple->save();
-});
